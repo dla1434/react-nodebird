a2b80c43c089b8b459a428df046b3a1306884bc0
lim jong hyun <dla1434@naver.com>
2021-04-25 00:20:12 +0900

    반응형그리드_row_col 사용하기

--- prepare_jh/front/components/AppLayout.js
+++ prepare_jh/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -18,11 +18,31 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.zerocho.com"
+            target="_blank"
+            rel="_noreferrer noopener"
+          >
+            Made by a zerocho
+          </a>
+        </Col>
+      </Row>
       {children}
     </>
   );
