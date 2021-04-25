a3a1a285e19b90689571c1b6ca6a0cce21d5d464
lim jong hyun <dla1434@naver.com>
2021-04-26 02:12:24 +0900

    global style과 style 파일 분리하기

--- prepare_jh/front/components/imagesZoom/index.js
+++ prepare_jh/front/components/imagesZoom/index.js
@@ -1,82 +1,15 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Slick from "react-slick";
-import styled, { createGlobalStyle } from "styled-components";
-
-const Overlay = styled.div`
-  position: fixed;
-  z-index: 5000;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-const Header = styled.header`
-  height: 44px;
-  background: white;
-  position: relative;
-  padding: 0;
-  text-align: center;
-
-  //header 안에 h1 태그를 의미
-  & h1 {
-    margin: 0;
-    font-size: 17px;
-    color: #333;
-    line-height: 44px;
-  }
-`;
-
-const SlickWrapper = styled.div`
-  height: calc(100% - 44px);
-  background: #090909;
-`;
-
-const CloseBtn = styled.button`
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 15px;
-  line-height: 14px;
-  cursor: pointer;
-`;
-
-const ImageWrapper = styled.div`
-  padding: 32px;
-  text-align: center;
-
-  & img {
-    margin: 0 auto;
-    max-height: 750px;
-  }
-`;
-
-const Indicator = styled.div`
-  text-align: center;
-
-  & > div {
-    width: 75px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 15px;
-    background: #313131;
-    display: inline-block;
-    text-align: center;
-    color: white;
-    font-size: 15px;
-  }
-`;
-
-const Global = createGlobalStyle`
-  .slick-slide {
-    display: inline-block; 
-
-  }
-  .ant-card-cover {
-    transform: none !important;
-  }
-`;
+import {
+  Overlay,
+  Global,
+  Header,
+  CloseBtn,
+  ImgWrapper,
+  Indicator,
+  SlickWrapper,
+} from "./styles";
 
 const ImageZoom = ({ images, onClose }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -92,18 +25,23 @@ const ImageZoom = ({ images, onClose }) => {
         <div>
           <Slick
             initialSlide={0}
-            afterChange={(slide) => setCurrentSlide(slide)}
+            beforeChange={(slide) => setCurrentSlide(slide)}
             infinite
             arrow={false}
             slidesToShow={1}
             slidesToScroll={1}
           >
             {images.map((v) => (
-              <ImageWrapper key={v.src}>
+              <ImgWrapper key={v.src}>
                 <img src={v.src} alt={v.src} />
-              </ImageWrapper>
+              </ImgWrapper>
             ))}
           </Slick>
+          <Indicator>
+            <div>
+              {currentSlide + 1}/{images.length}
+            </div>
+          </Indicator>
         </div>
       </SlickWrapper>
     </Overlay>
