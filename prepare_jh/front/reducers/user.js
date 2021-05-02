2ce20ff4298bf240dbc39b2339b48a9e93a8ce80
lim jong hyun <dla1434@naver.com>
2021-05-03 02:22:49 +0900

    reduce saga 기본 사용법 완료

--- prepare_jh/front/reducers/user.js
+++ prepare_jh/front/reducers/user.js
@@ -1,8 +1,49 @@
 export const initialState = {
+  isLoggingIn: false, //로그인 시도 중
+  isLoggingOut: false, //로그 아웃 시도 중
   isLoggedIn: false,
   me: null,
   signUpData: {},
   loginData: {},
+};
+
+export const loginRequestAction = (data) => {
+  return {
+    type: "LOG_IN_REQUEST",
+    data,
+  };
+};
+
+export const loginSuccessAction = (data) => {
+  return {
+    type: "LOG_IN_SUCCESS",
+    data,
+  };
+};
+
+export const loginFailureAction = (data) => {
+  return {
+    type: "LOG_IN_FAILURE",
+    data,
+  };
+};
+
+export const logoutRequestAction = () => {
+  return {
+    type: "LOG_OUT_REQUEST",
+  };
+};
+
+export const logoutSuccessAction = () => {
+  return {
+    type: "LOG_OUT_SUCCESS",
+  };
+};
+
+export const logoutFailureAction = () => {
+  return {
+    type: "LOG_OUT_FAILURE",
+  };
 };
 
 export const loginAction = (data) => {
@@ -22,6 +63,44 @@ export const logoutAction = () => {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case "LOG_IN_REQUEST":
+      console.log("reducer login");
+      return {
+        ...state,
+        isLoggingIn: true,
+        me: action.data,
+      };
+    case "LOG_IN_SUCCESS":
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        me: { ...action.data, nickname: "zerocho" },
+      };
+    case "LOG_IN_FAILURE":
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+      };
+    case "LOG_OUT_REQUEST":
+      return {
+        ...state,
+        isLoggingOut: true,
+      };
+    case "LOG_OUT_SUCCESS":
+      return {
+        ...state,
+        isLoggingOut: false,
+        isLoggedIn: false,
+        me: null,
+      };
+    case "LOG_OUT_FAILURE":
+      return {
+        ...state,
+        isLoggingOut: false,
+        isLoggedIn: false,
+      };
     case "LOG_IN":
       return {
         ...state,
--- prepare_jh/front/store/configureStore.js
+++ prepare_jh/front/store/configureStore.js
@@ -2,15 +2,26 @@ import { createWrapper } from "next-redux-wrapper";
 import reducer from "../reducers";
 import { createStore, compose, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+// import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas";
+
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+};
 
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddeleware = createSagaMiddleware();
+  const middlewares = [sagaMiddeleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddeleware.run(rootSaga);
+
   return store;
 };
 
