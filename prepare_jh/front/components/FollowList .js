292f62278e93fa6e1d6a21b065ea4ffbbb6c2e43
lim jong hyun <dla1434@naver.com>
2021-05-13 00:11:48 +0900

    팔로잉/팔로우 완료

--- prepare_jh/front/components/FollowList .js
+++ prepare_jh/front/components/FollowList .js
@@ -1,30 +1,51 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Card, List, Button } from "antd";
-import { StopOutlined } from "@ant-design/icons";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, List, Button } from 'antd';
+import { StopOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
+import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from '../reducers/user';
 
-const FollowList = ({ header, data }) => (
-  <List
-    style={{ marginBottom: 20 }}
-    grid={{ gutter: 4, xs: 2, md: 3 }}
-    size="small"
-    header={<div>{header}</div>}
-    loadMore={
-      <div style={{ textAlign: "center", margin: "10px 0" }}>
-        <Button>더 보기</Button>
-      </div>
+const FollowList = ({ header, data }) => {
+  const dispatch = useDispatch();
+  //함수에서 인자값을 받아서 처리할때는 고차함수를 사용
+  const onCancel = (id) => () => {
+    if (header === '팔로잉') {
+      dispatch({
+        type: UNFOLLOW_REQUEST,
+        data: id,
+      });
     }
-    bordered
-    dataSource={data}
-    renderItem={(item) => (
-      <List.Item style={{ marginTop: "20px" }}>
-        <Card actions={[<StopOutlined key="stop" />]}>
-          <Card.Meta description={item.nickname} />
-        </Card>
-      </List.Item>
-    )}
-  />
-);
+    dispatch({
+      type: REMOVE_FOLLOWER_REQUEST,
+      data: id,
+    });
+  };
+
+  return (
+    <List
+      style={{ marginBottom: 20 }}
+      grid={{ gutter: 4, xs: 2, md: 3 }}
+      size="small"
+      header={<div>{header}</div>}
+      loadMore={
+        <div style={{ textAlign: 'center', margin: '10px 0' }}>
+          <Button>더 보기</Button>
+        </div>
+      }
+      bordered
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item style={{ marginTop: '20px' }}>
+          <Card
+            actions={[<StopOutlined key="stop" onClick={onCancel(item.id)} />]}
+          >
+            <Card.Meta description={item.nickname} />
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
+};
 
 FollowList.propTypes = {
   header: PropTypes.string.isRequired,
